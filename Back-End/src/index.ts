4c7791bd75c2e935a0fe810290b90db0a04c9fd2
Mohamed Salah <midosalah25552@gmail.com>
2026-04-23 21:16:22 +0200

    resolved merge conflict

--- Back-End/src/index.ts
+++ Back-End/src/index.ts
@@ -8,6 +8,3 @@ app.listen(env.PORT, () => {
   console.log(`Environment: ${env.NODE_ENV}`);
   console.log(`Client URL: ${env.CLIENT_URL}`);
 });
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`))
